142c52c3a93e91567f7bb1bf501e4fbd2c651431
Ally Tiang <allytiang@Allys-MacBook-Pro.local>
2018-11-10 21:01:41 +1300

    init

---
+++ OnBoardingExample/Views/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  Slide.swift
+//  OnBoardingExample
+//
+//  Created by Ally Tiang on 11/10/18.
+//  Copyright © 2018 Ally Tiang. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ OnBoardingExample/Views/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OnBoardingExample
+//
+//  Created by Ally Tiang on 11/10/18.
+//  Copyright © 2018 Ally Tiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
